68939640df4657f2dc73034612a91894b5460de1
r-pluss <r.pluss52@gmail.com>
2016-01-18 15:53:29 -0500

    checking to see if credential.helper is working correctly

--- python/base.py
+++ python/base.py
@@ -7,3 +7,9 @@ from scipy.misc import imread
 class image_extractor():
   def __init__(self, path= None):
     self.img_dir= path
+    if self.img:
+        if os.path.isdir(self.img_dir):
+            #awesome
+            pass
+        else:
+            raise ValueError(str(self.img_dir) + ' is not a directory.')
